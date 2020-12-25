ca4410ecf801d4c11b626230932e281814f832b9
usmanabbas2211 <usmanabbas2211@gmail.com>
2020-12-24 19:30:54 -0800

    section-4

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './menu-item.style.scss';
+const MenuItem = ({ id, title, imageUrl, size }) => {
+    return (
+        <div className={`${size} menu-item`}>
+            <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}>
+
+            </div>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem
---
+++ src/pages/homepage/home.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directory from '../../components/directory/directory.component'
+import './home.style.scss';
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <Directory></Directory>
+        </div>
+    )
+}
+
+export default HomePage
